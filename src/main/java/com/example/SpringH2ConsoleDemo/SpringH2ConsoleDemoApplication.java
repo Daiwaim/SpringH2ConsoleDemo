87c8411973870ac039e92ffac8be54e7aeb42b56
Hugo Picollet <hugo.picollet@gmail.com>
2019-01-06 11:04:35 +0100

    Initial commit

---
+++ src/main/java/com/example/SpringH2ConsoleDemo/SpringH2ConsoleDemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.SpringH2ConsoleDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringH2ConsoleDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringH2ConsoleDemoApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/example/SpringH2ConsoleDemo/SpringH2ConsoleDemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.SpringH2ConsoleDemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringH2ConsoleDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
